61e1b28454861c2e224c54ed089e4205fd53bd8e
Luis Alfonso Pérez Ureña <34783400+Luisperez277@users.noreply.github.com>
2022-05-16 18:05:22 -0500

    Primer Aporte al Blog

---
+++ script.js
@@ -0,0 +1,12 @@
+alert("Estás ingresando a mi nuevo proyecto");
+        var nombre = "";
+        nombre = prompt("Bienvenido a mi Blogg, Cómo te llamas?");
+                
+        if (nombre == "Luis")
+        {
+        alert("Bienvenido " + nombre)
+        } 
+        else 
+        {
+        alert("Disculpa " + nombre + ", no estás registrado en nuestra base de datos.")  
+        }
